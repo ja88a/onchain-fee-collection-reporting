48c6e314fb6446df5e5c17f738c5b58ae79ec291
ja88a <r0g3r@tuta.io>
2025-06-10 13:58:14 +0200

    chore: remove the serverless envelop package made for events-scraper

--- packages/events-scraper-serverless/eslint.config.ts
+++
@@ -1,9 +0,0 @@
-import { Linter } from "eslint";
-import { config } from "@repo/eslint-config/base";
-
-export default [
-  ...config,
-  {
-    ignores: ["dist/**", ".turbo/**", "node_modules/**"],
-  },
-] satisfies Linter.Config[];
